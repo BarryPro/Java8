d6260e55c06967e8ff506354bdfc338acecee050
belong <belong.belong@outlook.com>
2017-07-04 13:42:21 +0800

    长时间没有备份了，备份一下

--- src/main/java/org/demo/others/Demo.java
+++ src/main/java/org/demo/others/Demo.java
@@ -1,13 +1,36 @@
 package org.demo.others;
 
-import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * Created by belong on 2017/3/28.
  */
 public class Demo {
-    public static void main(String[] args) {
-    	TreeMap<String, Object> treeMap;
-
-    }
+	public static void main(String[] args) {
+		URL url = Demo.class.getClassLoader().getResource("str.txt");
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(url.getPath()), "utf-8"));
+			String buffer = null;
+			String [] path = null;
+			int index = 0;
+			while((buffer=reader.readLine())!=null){
+//				path = buffer.replaceAll("[:、.,]", "：").split("[;；]");
+				path = buffer.split("[\\s]");
+				for(String s:path){
+					System.out.println("\""+"com.sitech.acctmgr.inter.invoice."+s.replaceAll(".java", "")+"\",");
+				}
+				
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
 }
